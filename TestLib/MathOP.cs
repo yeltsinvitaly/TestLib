afa6164cafbf27d347c8ab001a3c4e6bf1f2172b
yeltsinvitaly <yeltsinvitaly@gmail.com>
2019-10-11 04:10:20 +0300

    Добавьте файлы проекта.

---
+++ TestLib/MathOP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    public static class MathOP
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
